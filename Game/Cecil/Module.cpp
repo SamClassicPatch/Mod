5a9a94675af85a666af73a050d2ee4e2bcad1947
Dreamy Cecil <stalker27production@gmail.com>
2023-06-12 19:40:27 +0300

    Fill plugin module metadata for custom Game library.

---
+++ Game/Cecil/Module.cpp
@@ -0,0 +1,28 @@
+/* Copyright (c) 2023 Dreamy Cecil
+This program is free software; you can redistribute it and/or modify
+it under the terms of version 2 of the GNU General Public License as published by
+the Free Software Foundation
+
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+
+#include "StdAfx.h"
+
+// Retrieve module information
+MODULE_API void Module_GetInfo(CPluginAPI::PluginInfo *pInfo) {
+  // Don't set utility flags, so it doesn't get freed by the plugin API
+  pInfo->apiVer = CORE_API_VERSION;
+
+  // Metadata
+  pInfo->strAuthor = "Croteam, Dreamy Cecil";
+  pInfo->strName = "Game library";
+  pInfo->strDescription = "Main component that provides game logic.";
+  pInfo->ulVersion = CORE_PATCH_VERSION;
+};
